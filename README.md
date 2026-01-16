d60e4cdcb3d7cf217e76c80ac0b4946c21c17d62
PrakashNayak <prakashnayak1988@gmail.com>
2026-01-17 00:03:15 +0530

    Added the RAG Initial changes

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Employee RAG Platform
+
+One-click runnable Spring Boot + Spring AI RAG platform.
+
+## Run
+docker-compose up -d
+mvn clean install
---
+++ rag-service/src/main/java/com/example/rag/RagServiceApplication.java
@@ -0,0 +1,12 @@
+
+package RagServiceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RagServiceApplication.class, args);
+    }
+}
